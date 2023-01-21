6a83146b382f53655947f2c600c708d51a117f85
Harel Zadok <42193605+IShouldBeGood@users.noreply.github.com>
2023-01-21 05:52:14 +0200

    Added Mobile Support

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -23,16 +23,16 @@ function TopPart() {
     }, []);
 
     return <ScrollPage>
-        <div className={'body_main'}>
-            <small className={'bodyTitle_main'} style={{fontFamily: 'Segoe Print', fontSize: (scrollPosition / 100 + 1.6) + 'vw', top: 35.5 - scrollPosition / 5 + 'vh'}}>MY - Productions</small>
-            <small className={'bodyTitle_main'} style={{fontSize: (scrollPosition / 100 + 1.3) + 'vw', top: (40.6 - scrollPosition / 5.5) + 'vh'}}>"חלומות שהופכים מציאות"</small>
+        <div className={'body_main_d'}>
+            <small className={'bodyTitle_main_d'} style={{fontFamily: 'Segoe Print Bold', fontSize: (scrollPosition / 100 + 1.6) + 'vw', top: 35.5 - scrollPosition / 5 + 'vh'}}>MY - Productions</small>
+            <small className={'bodyTitle_main_d'} style={{fontSize: (scrollPosition / 100 + 1.3) + 'vw', top: (40.6 - scrollPosition / 5.5) + 'vh'}}>"חלומות שהופכים מציאות"</small>
             <Animator style={{height: '100%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}} animation={FadeUp}>
-                <small className={'bodyText_main'}>הפקות אירועים | השכרת ציוד לאירועים | ניהול אירועים</small>
-                <button className={'eventButton_main'}>בואו ונגשים פנטזיות</button>
+                <small className={'bodyText_main_d'}>הפקות אירועים | השכרת ציוד לאירועים | ניהול אירועים</small>
+                <button className={'eventButton_main_d'}>בואו ונגשים פנטזיות</button>
                 <button onClick={scrollToBottom} style={{backgroundColor: '#FFFFFF00', borderWidth: 0, cursor: 'pointer', position: 'absolute', bottom: '2vh', width: '100%', justifyContent: 'center', display: 'flex'}}>
                     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <small className={'contactUs_main'}>צור קשר</small>
-                        <img className={'arrowsDown_main'} src={arrowsDown} alt={'contact'}/>
+                        <small className={'contactUs_main_d'}>צור קשר</small>
+                        <img className={'arrowsDown_main_d'} src={arrowsDown} alt={'contact'}/>
                     </div>
                 </button>
             </Animator>
@@ -50,16 +50,16 @@ function getScrollPercent() {
 
 function BottomPart() {
     return <ScrollPage>
-        <div className={'footer_main'}>
+        <div className={'footer_main_d'}>
             <Animator animation={batch(MoveIn(900, 100), MoveOut(900, 100))}>
-                <div className={'extras_main'}>
-                    <div className={'extraList_main'}>
+                <div className={'extras_main_d'}>
+                    <div className={'extraList_main_d'}>
                         <Extra img={require('../resources/extra5.png')} text={'הפקות ברמה עולמית'}/>
                         <Extra img={require('../resources/extra7.png')} text={'זמינות'}/>
                         <Extra img={require('../resources/extra4.png')} text={'חשיבה מחוץ לקופסא'}/>
                         <Extra img={require('../resources/extra6.png')} text={'יצירת קשר עם ספקים'}/>
                     </div>
-                    <div className={'extraList_main'}>
+                    <div className={'extraList_main_d'}>
                         <Extra img={require('../resources/extra1.png')} text={'בכל מקום'}/>
                         <Extra img={require('../resources/extra2.png')} text={'פתרון בעיות'}/>
                         <Extra img={require('../resources/extra3.png')} text={'יעילות'}/>
@@ -69,12 +69,12 @@ function BottomPart() {
                 </div>
             </Animator>
             <Animator animation={MoveIn(-900, 100)}>
-                <div className={'contact_main'}>
-                    <button className={'contactButton_main'}><img className={'contactImage_main'} alt={'facebook'} src={require('../resources/facebook.png')}/></button>
-                    <button className={'contactButton_main'}><img className={'contactImage_main'} alt={'instagram'} src={require('../resources/instagram.png')}/></button>
-                    <button className={'contactButton_main'}><img className={'contactImage_main'} alt={'email'} src={require('../resources/email.png')}/></button>
-                    <button className={'contactButton_main'}><img className={'contactImage_main'} alt={'phone'} src={require('../resources/phone.png')}/></button>
-                    <button className={'contactButton_main'}><img className={'contactImage_main'} alt={'whatsapp'} src={require('../resources/whatsapp.png')}/></button>
+                <div className={'contact_main_d'}>
+                    <button className={'contactButton_main_d'}><img className={'contactImage_main_d'} alt={'facebook'} src={require('../resources/facebook.png')}/></button>
+                    <button className={'contactButton_main_d'}><img className={'contactImage_main_d'} alt={'instagram'} src={require('../resources/instagram.png')}/></button>
+                    <button className={'contactButton_main_d'}><img className={'contactImage_main_d'} alt={'email'} src={require('../resources/email.png')}/></button>
+                    <button className={'contactButton_main_d'}><img className={'contactImage_main_d'} alt={'phone'} src={require('../resources/phone.png')}/></button>
+                    <button className={'contactButton_main_d'}><img className={'contactImage_main_d'} alt={'whatsapp'} src={require('../resources/whatsapp.png')}/></button>
                 </div>
             </Animator>
         </div>
@@ -83,17 +83,33 @@ function BottomPart() {
 
 export default function MainPage()
 {
-    if (isMobile) {
-        return <h1>Mobile version TBD</h1>
-    }
-
-    return <div className={'container_main'}>
-        <ScrollContainer>
-            <TopPart/>
-            <BottomPart/>
-        </ScrollContainer>
-        <TopBar/>
-    </div>
+    if (isMobile)
+        return <div className={'container_main_m'}>
+            <div className={'topBar_main_m'}>
+                <button onClick={alert} className={'drawerButton_main_m'}>
+                    <img src={require('../resources/drawer.png')} alt={'drawer'} className={'drawer_main_m'}/>
+                </button>
+                <div className={'logoContainer_main_m'}>
+                    <img src={require('../resources/logo1.png')} alt={'logo'} className={'logo_main_m'}/>
+                </div>
+            </div>
+            <div className={'body_main_m'}>
+                <small className={'bodyTitle_main_m'} style={{fontFamily: 'Segoe Print Bold', top: '5vh'}}>MY - Productions</small>
+                <small className={'bodyTitle_main_m'} style={{top: '5vh'}}>"חלומות שהופכים מציאות"</small>
+                <button className={'eventButton_main_m'}>בואו ונגשים פנטזיות</button>
+                <small className={'bodyText_main_m'}>| הפקות אירועים</small>
+                <small className={'bodyText_main_m'}>| השכרת ציוד</small>
+                <small className={'bodyText_main_m'}>| ניהול אירועים</small>
+            </div>
+        </div>
+    else
+        return <div className={'container_main_d'}>
+            <ScrollContainer>
+                <TopPart/>
+                <BottomPart/>
+            </ScrollContainer>
+            <TopBar/>
+        </div>
 }
 
 function scrollToBottom() {
